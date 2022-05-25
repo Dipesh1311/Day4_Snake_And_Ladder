011b874bcc57177d151906eca39b2efd2e0ecd78
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-05-25 16:30:23 +0530

    Program for player to reach winning position

--- src/com/day4/SnakeAndLadder.java
+++ src/com/day4/SnakeAndLadder.java
@@ -10,36 +10,44 @@ public class SnakeAndLadder {
     final static int Ladder = 1;
     final static int Snake = 2;
     static int position = 0;
+    final static int WINPOINT = 100;
 
     public static void main(String[] args) {
-        System.out.println("Player1 at " +player1+ " position");
+        System.out.println("Lets Play Snake and Ladder");
         Random a = new Random();
+
+        do {
         Roll_Dice = a.nextInt(6) + 1;
-        System.out.println(Roll_Dice);
+        System.out.println("Number after dice roll : "+Roll_Dice);
         check = a.nextInt(3);
-        System.out.println(check);
+        System.out.println("Player option : "+check);
 
         switch (check) {
-            case   No_Play:
-                    System.out.println("NOPLAY");
-                    System.out.println("Position of player : " + position);
-                    System.out.println("----------------------");
-                    break;
+            case No_Play:
+                System.out.println("NOPLAY");
+                System.out.println("Position of player : " + position);
+                System.out.println("----------------------");
+                break;
             case Ladder:
-                    System.out.println("############## Climb up the ladder ############");
-                    position += Roll_Dice;
-                    System.out.println("Position of the  Player = " + position);
-                    System.out.println("----------------------");
+                System.out.println("############## Climb up the ladder ############");
+                position += Roll_Dice;
+                System.out.println("Position of the  Player = " + position);
+                System.out.println("----------------------");
                 break;
             case Snake:
-                    System.out.println("~~~~~~~~~~~~~~~~ Swallowed by snake ~~~~~~~~~~~~");
-                    position -= Roll_Dice;
-                    System.out.println("Position of the  Player = " + position);
-                    System.out.println("----------------------");
+                System.out.println("~~~~~~~~~~~~~~~~ Swallowed by snake ~~~~~~~~~~~~");
+                position -= Roll_Dice;
+                System.out.println("Position of the  Player = " + position);
+                System.out.println("----------------------");
                 break;
 
             default:
                 break;
-        }
+            }
+                if (position < 0) {
+                position = 0;
+                }
+        } while (position != WINPOINT);
     }
+
 }
