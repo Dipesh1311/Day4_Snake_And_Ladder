244b4dee894b55dd232fe07bb1e1dfa18815dfb0
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-05-25 17:05:03 +0530

    No. of times dice rolled

--- src/com/day4/SnakeAndLadder.java
+++ src/com/day4/SnakeAndLadder.java
@@ -3,67 +3,42 @@ package com.day4;
 import java.util.Random;
 
 public class SnakeAndLadder {
-    static int player1 = 0;
-    static int Roll_Dice;
-    static int check;
-    final static int No_Play = 0;
-    final static int Ladder = 1;
-    final static int Snake = 2;
-    static int position = 0;
-    final static int WINPOINT = 100;
-    public static int calculatedPosition = 0;
-
-    public static int calculatePlayerLadderPosition(int position, int dicevalue) {
-        position = position + dicevalue;
-        if (position > WINPOINT) {
-            position = position - dicevalue;
-        } else if (position == 100) {
-            return position;
-        }
-        return position;
-    }
-
-    public static int calculatePlayerSnakeBitePosition(int position, int dicevalue) {
-        position = position - dicevalue;
-        if (position < 0) {
-            position = 0;
-        }
-        return position;
-    }
+    public static final int startPosition = 0;
+    public static final int minPosition = 0;
+    public static final int maxPosition = 100;
+    public static final int noPlay=1;
+    public static final int ladder=2;
+    public static final int snake=3;
 
     public static void main(String[] args) {
-        System.out.println("Lets Play Snake and Ladder");
-        Random a = new Random();
 
-        do {
-        Roll_Dice = a.nextInt(6) + 1;
-        System.out.println("Number after dice roll : "+Roll_Dice);
-        check = a.nextInt(3);
-        System.out.println("Player option : "+check);
+        int playerPosition = startPosition;
+        int numOfDiceRolls = 0;
 
-        switch (check) {
-            case No_Play:
-                System.out.println("NOPLAY");
-                System.out.println("Position of player : " + position);
-                System.out.println("----------------------");
-                break;
-            case Ladder:
-                System.out.println("############## Climb up the ladder ############");
-                position += Roll_Dice;
-                System.out.println("Position of the  Player = " + position);
-                System.out.println("----------------------");
-                break;
-            case Snake:
-                System.out.println("~~~~~~~~~~~~~~~~ Swallowed by snake ~~~~~~~~~~~~");
-                position -= Roll_Dice;
-                System.out.println("Position of the  Player = " + position);
-                System.out.println("----------------------");
-                break;
+        System.out.println("Starting position of a Player is " + startPosition);
 
-            default:
-                break;
+        while(playerPosition < 100) {
+            int diceRoll = ((int)Math.floor(Math.random() * 10) % 6) + 1;
+            numOfDiceRolls++;
+            System.out.println("Rolling Dice: " + diceRoll);
+            int choice = (int)Math.floor(Math.random() * 10) % 3 + 1;
+            switch (choice) {
+                case noPlay: System.out.println("No Play");
+                    break;
+                case ladder: System.out.println("Ladder is there");
+                    if(playerPosition + diceRoll > maxPosition)
+                        System.out.println("Current Player Position: " + playerPosition);
+                    playerPosition = (playerPosition + diceRoll) <= maxPosition ? (playerPosition + diceRoll) : playerPosition;
+                    break;
+                case snake:  System.out.println("Snake is there");
+                    playerPosition = (playerPosition - diceRoll) >= minPosition ? (playerPosition - diceRoll) : minPosition;
+                    if(playerPosition == 0)
+                        System.out.println("Start from 0 position.");
+                    break;
+                default:
             }
-        } while (calculatedPosition != WINPOINT);
+            System.out.println("Latest Player Position is " + playerPosition);
+        }
+        System.out.println("Dice Rolled: " + numOfDiceRolls + " times to win the game.");
     }
-
 }
