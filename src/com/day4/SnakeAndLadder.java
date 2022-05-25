252bedda56e3ddb13fd9aab1fedf8aface1ee98a
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-05-25 16:09:33 +0530

    Player1 check for option Noplay,Ladder,Snake

--- src/com/day4/SnakeAndLadder.java
+++ src/com/day4/SnakeAndLadder.java
@@ -4,12 +4,42 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     static int player1 = 0;
-    static int Roll_Die;
+    static int Roll_Dice;
+    static int check;
+    final static int No_Play = 0;
+    final static int Ladder = 1;
+    final static int Snake = 2;
+    static int position = 0;
 
     public static void main(String[] args) {
         System.out.println("Player1 at " +player1+ " position");
         Random a = new Random();
-        Roll_Die = a.nextInt(6)+1;
-        System.out.println(Roll_Die);
+        Roll_Dice = a.nextInt(6) + 1;
+        System.out.println(Roll_Dice);
+        check = a.nextInt(3);
+        System.out.println(check);
+
+        switch (check) {
+            case   No_Play:
+                    System.out.println("NOPLAY");
+                    System.out.println("Position of player : " + position);
+                    System.out.println("----------------------");
+                    break;
+            case Ladder:
+                    System.out.println("############## Climb up the ladder ############");
+                    position += Roll_Dice;
+                    System.out.println("Position of the  Player = " + position);
+                    System.out.println("----------------------");
+                break;
+            case Snake:
+                    System.out.println("~~~~~~~~~~~~~~~~ Swallowed by snake ~~~~~~~~~~~~");
+                    position -= Roll_Dice;
+                    System.out.println("Position of the  Player = " + position);
+                    System.out.println("----------------------");
+                break;
+
+            default:
+                break;
+        }
     }
 }
