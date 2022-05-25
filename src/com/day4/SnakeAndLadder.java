d3f75ea8255cdd5cf913b2d30b0f1133a15e8b2c
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-05-25 15:23:12 +0530

    Start of Snake and ladder game

---
+++ src/com/day4/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.day4;
+
+public class SnakeAndLadder {
+    static int player1 = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Player1 at " +player1+ " position");
+    }
+}
