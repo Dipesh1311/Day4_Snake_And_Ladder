9d6a27a92b6c937f22f2fc77cd5816f09556e282
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-05-25 15:43:01 +0530

    Program for rolling die

--- src/com/day4/SnakeAndLadder.java
+++ src/com/day4/SnakeAndLadder.java
@@ -1,9 +1,15 @@
 package com.day4;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     static int player1 = 0;
+    static int Roll_Die;
 
     public static void main(String[] args) {
         System.out.println("Player1 at " +player1+ " position");
+        Random a = new Random();
+        Roll_Die = a.nextInt(6)+1;
+        System.out.println(Roll_Die);
     }
 }
